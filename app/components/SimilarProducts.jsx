ccd95d3840f79fc1f41ad5ef15d42567cd2a5c70
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-06-29 11:03:22 +0700

    Product page

--- app/components/SimilarProducts.jsx
+++ app/components/SimilarProducts.jsx
@@ -1,10 +1,9 @@
 'use client'
 
-import CarouselComp from './components/CarouselComp'
-import Product from './components/Product'
-import MainLayout from './layouts/MainLayout'
+import { BiLoader } from 'react-icons/bi'
+import ProductComp from './Product'
 
-export default function Home() {
+export default function SimilarProducts() {
   const products = [
     {
       id: 1,
@@ -26,17 +25,30 @@ export default function Home() {
 
   return (
     <>
-      <MainLayout>
-        <CarouselComp />
+      <div>
+        <div className='border-b py-1 max-w-[1200px] mx-auto' />
+
         <div className='max-w-[1200px] mx-auto'>
-          <div className='text-2xl font-bold mt-4 mb-6 px-4'>Products</div>
-          <div className='grid grid-cols-5 gap-4'>
-            {products.map((product) => (
-              <Product key={product.id} product={product} />
-            ))}
+          <div className='font-bold text-2xl py-2 mt-4'>
+            Similar Sponsored Items
           </div>
+
+          {products.length > 0 ? (
+            <div className='grid grid-cols-5 gap-4'>
+              {products.map((product) => (
+                <ProductComp key={product.id} product={product} />
+              ))}
+            </div>
+          ) : (
+            <div className='flex items-center justify-center'>
+              <div className='flex items-center justify-center gap-4 font-semibold'>
+                <BiLoader size={30} className='text-blue-400 animate-spin' />
+                Loading Products...
+              </div>
+            </div>
+          )}
         </div>
-      </MainLayout>
+      </div>
     </>
   )
 }
