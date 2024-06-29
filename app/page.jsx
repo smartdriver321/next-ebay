7465c3c85fc82bd2ba0bd6f3a6bc191538c926d8
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-06-29 09:58:31 +0700

    Carousel Component

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,14 @@
 'use client'
 
+import CarouselComp from './components/CarouselComp'
 import MainLayout from './layouts/MainLayout'
 
 export default function Home() {
   return (
     <>
-      <MainLayout>Cool</MainLayout>
+      <MainLayout>
+        <CarouselComp />
+      </MainLayout>
     </>
   )
 }
