28ae8226ba5f7661b6aec89cb5cec7afe854f06e
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-06-30 15:33:33 +0700

    Finishing the search input

--- app/layouts/includes/MainHeader.jsx
+++ app/layouts/includes/MainHeader.jsx
@@ -1,7 +1,42 @@
+'use client'
+
 import Link from 'next/link'
+import { useState } from 'react'
 import { AiOutlineSearch } from 'react-icons/ai'
+import { BiLoaderCircle } from 'react-icons/bi'
+import { debounce } from 'debounce'
 
 export default function MainHeader() {
+  const [items, setItems] = useState([])
+  const [isSearching, setIsSearching] = useState(null)
+
+  const handleSearchName = debounce(async (event) => {
+    if (event.target.value == '') {
+      setItems([])
+      return
+    }
+
+    setIsSearching(true)
+
+    try {
+      const response = await fetch(
+        `/api/products/search-by-name/${event.target.value}`
+      )
+      const result = await response.json()
+
+      if (result) {
+        setItems(result)
+        setIsSearching(false)
+        return
+      }
+      setItems([])
+      setIsSearching(false)
+    } catch (error) {
+      console.log(error)
+      alert(error)
+    }
+  }, 500)
+
   return (
     <>
       <div id='MainHeader' className='border-b'>
@@ -24,7 +59,43 @@ export default function MainHeader() {
                         className='w-full  placeholder-gray-400 text-sm pl-3 focus:outline-none'
                         placeholder='Search for anything'
                         type='text'
+                        onChange={handleSearchName}
                       />
+
+                      {isSearching ? (
+                        <BiLoaderCircle
+                          className='mr-2 animate-spin'
+                          size={22}
+                        />
+                      ) : null}
+
+                      {items.length > 0 ? (
+                        <div className='absolute bg-white max-w-[910px] h-auto w-full z-20 left-0 top-12 border p-1'>
+                          {items.map((item) => (
+                            <div className='p-1' key={item.id}>
+                              <Link
+                                href={`/product/${item?.id}`}
+                                className='flex items-center justify-between w-full cursor-pointer hover:bg-gray-200 p-1 px-2'
+                              >
+                                <div className='flex items-center'>
+                                  <img
+                                    className='rounded-md'
+                                    width='40'
+                                    src={item?.url + '/40'}
+                                    alt='img'
+                                  />
+                                  <div className='truncate ml-2'>
+                                    {item?.title}
+                                  </div>
+                                </div>
+                                <div className='truncate'>
+                                  £{(item?.price / 100).toFixed(2)}
+                                </div>
+                              </Link>
+                            </div>
+                          ))}
+                        </div>
+                      ) : null}
                     </div>
 
                     <button className='flex items-center bg-blue-600 text-sm font-semibold text-white p-[11px] ml-2 px-14'>
